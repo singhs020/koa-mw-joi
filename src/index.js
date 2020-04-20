0472bae125ca1a725fc15ef8a311c3c220b5f6af
Simranjeet Singh <simranjeet2589@gmail.com>
2020-04-20 12:14:14 +0530

    [adding getValidateBodyMw]

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const assert = require("assert");
 
 const createMw = require("./middleware");
 
-function getMw(schema) {
+function getValidateBodyMw(schema) {
   assert(schema && schema.isJoi === true, "The joi schema is required.");
   return createMw(schema);
 }
 
-module.exports = getMw;
+module.exports = {
+  getValidateBodyMw
+};
